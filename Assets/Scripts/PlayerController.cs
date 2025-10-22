68b4976ce20c3ac19b6741e6dc98e9ebd1af5d1b
Dawid Paluch <dawid1paluch@gmail.com>
2025-10-22 14:22:42 +0100

    fixing commit isuses

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private CharacterController Controller;
+    private HungerSystem hungerSystem; 
 
     private Vector3 playerVelocity;
     public float speed = 5f;
@@ -12,26 +13,43 @@ public class PlayerController : MonoBehaviour
     public float Gravity = -9.8f;
     public float JumpHeight = 1.5f;
     private CameraMovement camMove;
+    
+    private bool isMoving = false; 
+
     void Start()
     {
         Controller = GetComponent<CharacterController>();
         camMove = GetComponent<CameraMovement>();
+        hungerSystem = GetComponent<HungerSystem>();
+        if (hungerSystem == null)
+        {
+            Debug.LogError("HungerSystem script not found on the Player!");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        isGrounded = Controller.isGrounded; 
+        isGrounded = Controller.isGrounded;
+        
+        if (isMoving && isGrounded)
+        {
+            float depletionRate = (speed > 5f) 
+                                ? hungerSystem.sprintDepletionRate 
+                                : hungerSystem.walkDepletionRate;
+            
+            hungerSystem.DepleteHunger(depletionRate * Time.deltaTime);
+        }
     }
 
-
-    //recieves inputs from InputManager and applies it to char controller
     public void Move(Vector2 input)
     {
         Vector3 moveDir = Vector3.zero;
 
         moveDir.x = input.x;
         moveDir.z = input.y;
+        
+        isMoving = moveDir.magnitude > 0.1f;
+
         Controller.Move(transform.TransformDirection(moveDir) * speed * Time.deltaTime);
         playerVelocity.y += Gravity * Time.deltaTime;
         if (isGrounded && playerVelocity.y < 0) {
@@ -50,8 +68,7 @@ public class PlayerController : MonoBehaviour
 
     public void StartSprinting()
     {
-            
-            speed = 8f;
+        speed = 8f;
         camMove.isSprinting = true;
     }
 
